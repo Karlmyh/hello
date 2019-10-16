943a8827b6f7df2cbb673beb432d381d0d5a3775
Karlmayuheng <ym152@duke.edu>
2019-10-16 17:24:23 -0400

    First Commit

---
+++ 2/2/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  2
+//
+//  Created by 马宇恒 on 2019/10/16.
+//  Copyright © 2019 马宇恒. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
